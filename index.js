f616677455f73f429ac6ba448dd672a897dd7fac
Abdo <abdorabee1134@gmail.com>
2022-03-19 10:38:38 -0400

    added the express and the request and response methods

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 const express = require("express");
 const request = require("request-promise");
 
-const app =  express();
+const app = express();
 const PORT = process.env.PORT || 5000;
+
+const apiKey = "e41c88df35c52167a29a7072c0ac10c6";
+const baseUrl = `http://api.scaperapi.com?api_key=${apiKey}&autoparse=true`;
 
 app.use(express.json());
 
-app.get('/',(req, res) => {
-    res.send("Welcome to Amazon Scraper api by Abdo");
+app.get("/", (req, res) => {
+  res.send("Welcome to Amazon Scraper api by Abdo");
 });
 
-app.listen(PORT,()=>console.log(`Server running on port ${PORT}`));
+// GET Product Details
+app.get("/products/:productId", async (req, res) => {
+  const { productId } = req.params;
+
+  try {
+    const response = await request(
+      `${baseUrl}&url=http://www.amazon.com/dp/${productId}`
+    );
+    res.json(response);
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
