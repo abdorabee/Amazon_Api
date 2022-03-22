d323a3a833b75268df71d7cf7a7a63c28737e413
Abdo <abdorabee1134@gmail.com>
2022-03-22 12:58:03 -0400

    added the get offer response

--- index.js
+++ index.js
@@ -45,4 +45,18 @@ app.get("/products/:productId", async (req, res) => {
  });
  
 
+ // GET  Product offers
+ app.get("/products/:productId/offers", async (req, res) => {
+  const { productId } = req.params;
+
+  try {
+    const response = await request(
+      `${baseUrl}&url=http://www.amazon.com/gp/offer-listing/${productId}`
+    );
+    res.json(JSON.parse(response));
+  } catch (error) {
+    res.json(error);
+  }
+});
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
